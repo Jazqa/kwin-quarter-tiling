84762dd9411c017ccbb7029640b3c359e5d55f92
Jasu Koponen <jasu.koponen@hotmail.com>
2017-07-07 21:54:04 +0300

    renamed installation script

--- installation.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-plasmapkg2 --type kwinscript -i .
-mkdir -p ~/.local/share/kservices5
-cp ~/.local/share/kwin/scripts/quarter-tiling/metadata.desktop ~/.local/share/kservices5/kwin-script-quarter-tiling.desktop
