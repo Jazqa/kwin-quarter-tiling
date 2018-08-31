a9b2b4c4716c863c932dbe5e9b1d6571b34d1d9d
Jasu Koponen <jasu.koponen@hotmail.com>
2018-08-31 22:07:22 +0300

    Start refactoring and simplifying the script, starting with quarter layout

--- contents/code/main.js
+++ contents/code/main.js
@@ -32,13 +32,13 @@ function initScreens() {
   for (var i = 0; i < workspace.numScreens; i++) {
     switch (layout) {
       case "1":
-        screens[i] = new Tall(i);
+        screens[i] = new Quarter(i);
         break;
-      case "2":
-        screens[i] = new Tall(i);
-        break;
+      // case "2":
+      //  screens[i] = new Quarter(i);
+      //  break;
       default:
-        screens[i] = new Tall(i);
+        screens[i] = new Quarter(i);
     }
   }
 }
@@ -152,7 +152,7 @@ function findClient(client, array) {
 }
 
 // Saves a snapshot of the screen when moving starts
-const snapshot = {};
+var snapshot = {};
 
 function startMoveClient(client) {
   snapshot.geometry = client.geometry;
@@ -161,43 +161,46 @@ function startMoveClient(client) {
   snapshot.tiles = screens[snapshot.screen].getTiles(snapshot.windows.length);
 }
 
+function findClosestClient(client) {
+  var closest = [-1, 9999];
+  for (var i = 0; i < snapshot.tiles.length; i++) {
+    const distance =
+      Math.abs(client.geometry.x - snapshot.tiles[i].x) + Math.abs(client.geometry.y - snapshot.tiles[i].y);
+    if (distance < closest[1]) {
+      closest = [i, distance];
+    }
+  }
+  return closest;
+}
+
 function finishMoveClient(client) {
   const index = findClient(client, snapshot.windows);
+
   if (index > -1) {
     if (client.screen === snapshot.screen) {
       if (client.geometry.width === snapshot.geometry.width && client.geometry.height === snapshot.geometry.height) {
-        // Calculate the closest tile
-        var closest = [-1, 9999];
-        for (var i = 0; i < snapshot.tiles.length; i++) {
-          const distance =
-            Math.abs(client.geometry.x - snapshot.tiles[i].x) + Math.abs(client.geometry.y - snapshot.tiles[i].y);
-          if (distance < closest[1]) {
-            closest = [i, distance];
-          }
-        }
-        if (index !== closest[0]) {
-          // Swap with the closest
-          swapClients(findClient(snapshot.windows[index], windows), findClient(snapshot.windows[closest[0]], windows));
-        }
-        screens[snapshot.screen].tile();
+        // Moves the client
+        swapClients(
+          findClient(snapshot.windows[index], windows),
+          findClient(snapshot.windows[findClosestClient(client)[0]], windows)
+        );
       } else {
-        // Resize
+        // Resizes the client
         screens[snapshot.screen].resize(client);
       }
     } else {
-      // Push the client to the end of the array
+      // Pushes the client to the end of the array
       windows.push(windows.splice(index, 1)[0]);
-      screens[snapshot.screen].tile();
-      screens[client.screen].tile();
     }
   }
+
+  tileClients();
 }
 
 function changeDesktop(client, desktop) {
-  print("Changing desktop");
-
   if (client) {
     const index = findClient(client, windows);
+
     if (index > -1) {
       // Push the client to the end of the array
       windows.push(windows.splice(index, 1)[0]);
@@ -208,13 +211,14 @@ function changeDesktop(client, desktop) {
 }
 
 function swapClients(i, j) {
-  var t = windows[i];
-  windows[i] = windows[j];
-  windows[j] = t;
+  if (i !== j) {
+    var t = windows[i];
+    windows[i] = windows[j];
+    windows[j] = t;
+  }
 }
 
 function tileClients() {
-  print("Tiling clients");
   for (var i = 0; i < screens.length; i++) {
     screens[i].tile();
   }
@@ -251,191 +255,9 @@ registerShortcut("Quarter: Decrease Gap Size", "Quarter: Decrease Gap Size", "Me
   adjustGapSize(-2);
 });
 
-registerShortcut("Quarter: Switch Active With Master", "Quarter: Switch Active With Master", "Meta+Enter", function() {
-  swapClients(
-    findClient(workspace.activeClient, windows),
-    findClient(screens[workspace.activeClient.screen].getWindows()[0], windows)
-  );
-  screens[workspace.activeClient.screen].tile();
-});
-
-registerShortcut(
-  "Quarter: Increase Master Pane Capacity",
-  "Quarter: Increase Master Pane Capacity",
-  "Meta+<",
-  function() {
-    screens[workspace.activeScreen].adjustMaster(true);
-  }
-);
-
-registerShortcut(
-  "Quarter: Decrease Master Pane Capacity",
-  "Quarter: Decrease Master Pane Capacity",
-  "Meta+>",
-  function() {
-    screens[workspace.activeScreen].adjustMaster(false);
-  }
-);
-
 workspace.clientList().forEach(addClient);
 
-// Tall layout with two columns
-function Tall(i) {
+// Original Quarter layout
+function Quarter(i) {
   const id = i;
-  const masters = [];
-  const panes = [];
-
-  this.getMaster = function() {
-    const location = workspace.currentActivity.toString() + workspace.currentDesktop.toString();
-    masters[location] = masters[location] ? masters[location] : { n: 1 };
-    return masters[location];
-  };
-
-  this.getPane = function() {
-    const location = workspace.currentActivity.toString() + workspace.currentDesktop.toString();
-    panes[location] = panes[location] ? panes[location] : { x: [], y: [] };
-    return panes[location];
-  };
-
-  this.getGeometry = function(gaps) {
-    const availGeo = workspace.clientArea(0, id, workspace.currentDesktop);
-    const fullGeo = workspace.clientArea(1, id, workspace.currentDesktop);
-
-    if (fullGeo.x < availGeo.x) {
-      availGeo.width += availGeo.x - fullGeo.x;
-    }
-
-    if (fullGeo.y < availGeo.y) {
-      availGeo.height += availGeo.y - fullGeo.y;
-    }
-
-    if (gaps) {
-      availGeo.x += gap;
-      availGeo.y += gap;
-      availGeo.width -= gap * 2;
-      availGeo.height -= gap * 2;
-    }
-
-    return availGeo;
-  };
-
-  this.adjustMaster = function(increase) {
-    const master = this.getMaster();
-
-    if (increase) {
-      master.n += 1;
-    } else {
-      master.n = master.n > 1 ? master.n - 1 : 1;
-    }
-
-    this.tile();
-  };
-
-  this.getTiles = function(length) {
-    const geometry = this.getGeometry(true);
-    const master = this.getMaster().n;
-    const pane = this.getPane();
-
-    const tiles = [];
-
-    var columns = length > master ? 2 : 1;
-    for (var i = 0; i < columns; i++) {
-      var x = geometry.x + (geometry.width / columns) * i;
-      if (i !== 0 && pane.x[i]) {
-        x += pane.x[i];
-      }
-
-      var width;
-      if (i === columns - 1) {
-        width = geometry.width - x + gap;
-      } else {
-        var xn = (geometry.x + geometry.width / columns) * (i + 1);
-        xn += pane.x[i + 1] ? pane.x[i + 1] : 0;
-        width = xn - x - gap * i;
-      }
-
-      var rows = i === 0 ? master : length - master;
-      for (var j = 0; j < rows; j++) {
-        const k = j + master;
-
-        var y = geometry.y + (geometry.height / rows) * j;
-        if (j !== 0 && pane.y[k]) {
-          y += pane.y[k];
-        }
-
-        var height;
-        if (j === rows - 1) {
-          height = geometry.height - y + gap;
-        } else {
-          var yn = (geometry.y + geometry.height / rows) * (j + 1);
-          yn += pane.y[k + 1] ? pane.y[k + 1] : 0;
-          height = yn - y - gap * j;
-        }
-
-        tiles.push({
-          x: x,
-          y: y,
-          width: width,
-          height: height
-        });
-      }
-    }
-
-    return tiles;
-  };
-
-  this.getWindows = function() {
-    const included = windows.filter(function(window) {
-      return (
-        window.activities.indexOf(workspace.currentActivity > -1) &&
-        window.desktop === workspace.currentDesktop &&
-        window.screen === id
-      );
-    });
-
-    return included;
-  };
-
-  this.tile = function() {
-    const included = this.getWindows();
-    const tiles = this.getTiles(included.length);
-    for (var i = 0; i < included.length; i++) {
-      const tile = tiles[i];
-      tile.x += gap;
-      tile.y += gap;
-      tile.width -= gap * 2;
-      tile.height -= gap * 2;
-      included[i].geometry = tile;
-    }
-  };
-
-  this.resize = function(client) {
-    const included = this.getWindows();
-    const index = findClient(client, included);
-
-    if (index > -1) {
-      const pane = this.getPane();
-      var column = index < this.getMaster().n ? 0 : 1;
-
-      // Width
-      if (client.geometry.x !== snapshot.geometry.x) {
-        pane.x[column] = pane.x[column] ? pane.x[column] : 0;
-        pane.x[column] += client.geometry.x - snapshot.geometry.x;
-      } else {
-        pane.x[column + 1] = pane.x[column + 1] ? pane.x[column + 1] : 0;
-        pane.x[column + 1] += client.geometry.width - snapshot.geometry.width;
-      }
-
-      //Height
-      if (client.geometry.y !== snapshot.geometry.y) {
-        pane.y[index] = pane.y[index] ? pane.y[index] : 0;
-        pane.y[index] += client.geometry.y - snapshot.geometry.y;
-      } else {
-        pane.y[index + 1] = pane.y[index + 1] ? pane.y[index + 1] : 0;
-        pane.y[index + 1] += client.geometry.height - snapshot.geometry.height;
-      }
-
-      this.tile();
-    }
-  };
 }
