6d19cec6dd354733bb8483ab075901d081d3e7b0
Jasu Koponen <jasu.koponen@tieto.com>
2019-11-15 21:26:56 +0200

    babel and rollup conf

---
+++ contents/code/main.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var _qtClientManager = require("./qtClientManager");
+
+var _qtClientManager2 = _interopRequireDefault(_qtClientManager);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
